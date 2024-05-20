d50ae2c62c0f346658c726922c422897ae90ba6a
Javier E Montes <jaxloz2002@gmail.com>
2024-05-20 18:44:10 -0500

    ajustes en las rutas que requieren autenticacion

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,21 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
-    
-    Route::apiResource('faculties', FacultyController::class);
-    Route::apiResource('careers', CareerController::class);
-    Route::apiResource('semesters', SemesterController::class);
-    Route::apiResource('students', StudentController::class);
-    Route::apiResource('priorities', PriorityController::class);
-    Route::apiResource('statuses', StatusController::class);
-    Route::apiResource('tasks', TaskController::class);
+
+Route::prefix('v1')->group(function () {
+
+    Route::get('faculties', [FacultyController::class, 'index']);
+    Route::get('careers', [FacultyController::class, 'index']);
+    Route::get('semesters', [SemesterController::class, 'index']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+
+        Route::apiResource('faculties', FacultyController::class)->only(['store', 'show', 'update', 'destroy']);
+        Route::apiResource('careers', CareerController::class)->only(['store', 'show', 'update', 'destroy']);
+        Route::apiResource('semesters', SemesterController::class)->only(['store', 'show', 'update', 'destroy'])    ;
+        Route::apiResource('students', StudentController::class);
+        Route::apiResource('priorities', PriorityController::class);
+        Route::apiResource('statuses', StatusController::class);
+        Route::apiResource('tasks', TaskController::class);
+    });
 });
